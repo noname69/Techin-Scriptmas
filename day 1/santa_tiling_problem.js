69c4569b6abf3a9e76720ae1b9156634c7b83fbc
Pavel <Pavel.Stefanovic@gmail.com>
2025-12-02 06:59:17 +0200

    Day 1 - improvements

--- day 1/santa_tiling_problem.js
+++ day 1/santa_tiling_problem.js
@@ -4,4 +4,31 @@ function calculateTotalSantaCost(length, width, m2price) {
     var totalCost = areaWithExtra * m2price;
     return Number(totalCost.toFixed(2));
 }
-console.log(calculateTotalSantaCost(110, 50, 20.33));
+function showHelp() {
+    console.log("Some help");
+}
+var args = process.argv.slice(2);
+console.log(args);
+if (args.length !== 3) {
+    console.error("Need 3 arguments: lenght, width, price for squere meter");
+    showHelp();
+    process.exit(1);
+}
+var lenght = Number(args[0]);
+var width = Number(args[1]);
+var m2price = Number(args[2]);
+if (isNaN(lenght) || isNaN(width) || isNaN(m2price)) {
+    console.error("Arguments should be numbers");
+    showHelp();
+    process.exit(1);
+}
+try {
+    var result = calculateTotalSantaCost(lenght, width, m2price);
+    console.log("Total price: ", result);
+}
+catch (error) {
+    if (error instanceof Error) {
+        console.error("Error:", error.message);
+    }
+    process.exit(1);
+}
--- day 1/santa_tiling_problem.ts
+++ day 1/santa_tiling_problem.ts
@@ -6,4 +6,38 @@ function calculateTotalSantaCost(length: number, width: number, m2price: number)
   return Number(totalCost.toFixed(2));
 }
 
-console.log(calculateTotalSantaCost(110, 50, 20.33));
+function showHelp() {
+  console.log("Some help")
+}
+
+const args = process.argv.slice(2);
+console.log(args)
+
+if (args.length !== 3) {
+  
+  console.error("Need 3 arguments: lenght, width, price for squere meter");
+  showHelp();
+  process.exit(1);
+}
+
+const lenght = Number(args[0]);
+const width = Number(args[1]);
+const m2price = Number(args[2]);
+
+if (isNaN(lenght) || isNaN(width) || isNaN(m2price)) {
+  console.error("Arguments should be numbers");
+  showHelp();
+  process.exit(1);
+}
+
+
+try {
+  const result = calculateTotalSantaCost(lenght, width, m2price);
+  console.log("Total price: ", result);
+ 
+} catch (error) {
+  if (error instanceof Error) {
+    console.error("Error:", error.message);
+  }
+  process.exit(1);
+}
