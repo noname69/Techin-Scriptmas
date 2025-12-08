4f2643115e4a2fa86b478986a3f0491cbae99a17
Pavel <Pavel.Stefanovic@gmail.com>
2025-12-08 22:51:57 +0200

    Change CLI style, changed method of conversation of arguments from string to number and do validation or they are numbers

--- day 1/cli.ts
+++ day 1/cli.ts
@@ -1,42 +1,46 @@
 import { calculateTotalSantaCost } from "./santa_tiling_problem";
-/**
- * Function to show help message
- */
 
 function showHelp() {
   console.log("Usage:");
-  console.log("npx ts-node cli.ts <lenght> <width> <m2price>");
+  console.log("   npx ts-node cli.ts <lenght> <width> <m2price>");
   console.log("where <lenght>, <width>, <m2price> are numbers");
 }
 
-// Get command line arguments and validate
-const args = process.argv.slice(2);
+function main() {
+  try {
+    const args = process.argv.slice(2);
 
-if (args.length !== 3) {
-  console.error("Need 3 arguments: lenght, width, price for squere meter");
-  showHelp();
-  process.exit(1);
-}
+    if (args.length !== 3) {
+      throw new Error(
+        "Need 3 arguments: lenght, width, price for squere meter"
+      );
+    }
 
-const [lenght, width, m2price] = args.map(Number) as [number, number, number];
+    const [lengthStr, widthStr, m2priceStr] = args;
+    const length = Number(lengthStr);
+    const width = Number(widthStr);
+    const m2price = Number(m2priceStr);
 
-if (isNaN(lenght) || isNaN(width) || isNaN(m2price)) {
-  console.error("Arguments should be numbers");
-  showHelp();
-  process.exit(1);
-}
+    if ([length, width, m2price].some(Number.isNaN)) {
+      throw new Error("All arguments must be a number");
+    }
 
-// Calculate and display total Santa tiling price
-try {
-  const result = calculateTotalSantaCost(lenght, width, m2price);
-  if (result === 0) {
-    console.log("Your tiles are free!");
-    process.exit(0);
+    const result = calculateTotalSantaCost(length, width, m2price);
+
+    if (result === 0) {
+      console.log("Your tiles are free!");
+    } else {
+      console.log("Total price: ", result);
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("Error:", error.message);
+      showHelp();
+    } else {
+      console.error("Enexpected error occurred.");
+    }
+    process.exit(1);
   }
-  console.log("Total price: ", result);
-} catch (error) {
-  if (error instanceof Error) {
-    console.error("Error:", error.message);
-  }
-  process.exit(1);
 }
+
+main();
