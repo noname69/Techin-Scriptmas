9c0f9ad7b3448b21e32e5d1e943e20516f2c3970
Pavel <Pavel.Stefanovic@gmail.com>
2025-12-10 22:49:04 +0200

    update: help without arguments

--- utils/showHelp.ts
+++ utils/showHelp.ts
@@ -1,4 +1,8 @@
-export function showHelp(args: string[]) {
-  console.log(`Usage: npx ts-node cli.ts <${args.join("> <")}>`);
-  console.log(`       where <${args.join("> <")}> are numbers`);
+export function showHelp(args?: string[]) {
+  if (args) {
+    console.log(`Usage: npx ts-node cli.ts <${args?.join("> <")}>`);
+    console.log(`       where <${args?.join("> <")}> are numbers`);
+  } else {
+    console.log("Usage: npx ts-node cli.ts");
+  }
 }
