efa66bb81169af06db102bf0307129699eeb29d3
Pavel <Pavel.Stefanovic@gmail.com>
2025-12-10 22:15:06 +0200

    update: start to use utils functions: numberValidator and showHelp

--- day 1/cli.ts
+++ day 1/cli.ts
@@ -1,15 +1,14 @@
+import { numberValidator } from "../utils/numberValidator";
+import { showHelp } from "../utils/showHelp";
+
 import { calculateTotalSantaCost } from "./santa_tiling_problem";
 
-function showHelp() {
-  console.log("Usage:");
-  console.log("   npx ts-node cli.ts <lenght> <width> <m2price>");
-  console.log("where <lenght>, <width>, <m2price> are numbers");
-}
-
+//Main function
 function main() {
   try {
     const args = process.argv.slice(2);
 
+    //Check if there are 3 arguments
     if (args.length !== 3) {
       throw new Error(
         "Need 3 arguments: lenght, width, price for squere meter"
@@ -17,25 +16,32 @@ function main() {
     }
 
     const [lengthStr, widthStr, m2priceStr] = args;
-    const length = Number(lengthStr);
-    const width = Number(widthStr);
-    const m2price = Number(m2priceStr);
 
-    if ([length, width, m2price].some(Number.isNaN)) {
-      throw new Error("All arguments must be a number");
-    }
+    //Validate arguments
+    const length = numberValidator(lengthStr, {
+      name: "Length",
+      positive: true,
+    });
+    const width = numberValidator(widthStr, { name: "Width", positive: true });
+    const m2price = numberValidator(m2priceStr, {
+      name: "Price",
+      positive: true,
+    });
 
-    const result = calculateTotalSantaCost(length, width, m2price);
+    //Calculate total cost
+    const totalCost = calculateTotalSantaCost(length, width, m2price);
 
-    if (result === 0) {
+    //Output result
+    if (totalCost === 0) {
       console.log("Your tiles are free!");
     } else {
-      console.log("Total price: ", result);
+      console.log("Total cost:", totalCost);
     }
   } catch (error) {
+    //Handle errors
     if (error instanceof Error) {
       console.error("Error:", error.message);
-      showHelp();
+      showHelp(["length", "width", "price"]);
     } else {
       console.error("Enexpected error occurred.");
     }
--- day 1/santa_tiling_problem.ts
+++ day 1/santa_tiling_problem.ts
@@ -1,3 +1,4 @@
+import { numberValidator } from "../utils/numberValidator";
 /**
  * Function to calculate the total cost of tiling a floor for Santa's workshop,
  * including an extra 5% of tiles
@@ -7,19 +8,12 @@
  * @param extra - extra tiles in pronteges, default 5
  * @returns total rounded cost
  */
-
 export function calculateTotalSantaCost(
   length: number,
   width: number,
   m2price: number,
   extra = 5
 ): number {
-  if (length <= 0 || width <= 0 || m2price < 0) {
-    throw new Error(
-      "Length and width must be positive numbers, price cannot be negative"
-    );
-  }
-
   const area = length * width;
   const areaWithExtra = area * (1 + extra / 100);
   const totalCost = areaWithExtra * m2price;
